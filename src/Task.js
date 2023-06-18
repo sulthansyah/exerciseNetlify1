332ba34ac337290f1d33782e8197a063d2c7ae95
Sulthan Ahmad <Sulthanahmad41@gmail.com>
2023-06-18 14:27:10 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Sulthan Ahmad Ferdiansyah" // TODO: replace this
+const studentId = "FE5478582" // TODO: replace this
+
+export { url, studentName, studentId }
